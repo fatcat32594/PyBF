56197457d25da1e2e709ad02d805cd05c728e26f
Steve Hardy <fatcat32594@gmail.com>
2017-09-27 16:06:09 -0400

    Basic interpreter done

--- PyBF.py
+++ PyBF.py
@@ -27,6 +27,65 @@ def read(mode):
         sanatized_program = sanatize(program)
         return sanatized_program
 
+def jump(inst_ptr, program, direction):
+    """Jump the instruction pointer in the program until matching bracket"""
+    count = direction
+    while count != 0:
+        inst_ptr += direction
+        char = program[inst_ptr]
+        if char == '[':
+            count += 1
+        elif char == ']':
+            count -= 1
+        else:
+            pass
+    return inst_ptr
+
+def interpret(program):
+    """Interpret the program"""
+    inst_ptr = 0
+    data_ptr = 0
+    data = [0]
+
+    while inst_ptr < len(program):
+        command = program[inst_ptr]
+        if command == '<':
+            #Decrement the data pointer
+            data_ptr -= 1
+            if data_ptr < 0:
+                print("Error: attempting to go before data start")
+                quit()
+        elif command == '>':
+            #Increment the data pointer
+            data_ptr += 1
+            if data_ptr == len(data):
+                data += [0]
+        elif command == '+':
+            #Increment the data at the data pointer
+            data[data_ptr] += 1
+        elif command == '-':
+            #Decrement the data at the data pointer
+            data[data_ptr] -= 1
+        elif command == '.':
+            #Print the data at the data pointer as a character
+            print(chr(data[data_ptr]), end='')
+        elif command == ',':
+            #Read in one character from stdin to the data pointer location
+            char = sys.stdin.read(1)
+            data[data_ptr] = ord(char)
+        elif command == '[':
+            #Start of while loop
+            if data[data_ptr] == 0:
+                inst_ptr = jump(inst_ptr, program, 1)
+        elif command == ']':
+            #End of while loop
+            if data[data_ptr] != 0:
+                inst_ptr = jump(inst_ptr, program, -1)
+        else:
+            print("Error while interpreting program.")
+            exit()
+        inst_ptr += 1
+
 def main():
     """Main function"""
     arglen = len(sys.argv)
@@ -38,7 +97,7 @@ def main():
         print("Number of arguments not understood")
         exit()
     program = read(mode)
-    print(program)
+    interpret(program)
 
 if __name__ == "__main__":
     main()
